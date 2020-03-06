04d2f07fc1a8d9557fbd51236cfacbc5c047319a
Nick Elovsky <echonok@gmail.com>
2020-03-06 21:56:39 +0300

    change to public

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  localdb: 'mongodb://localhost:27017/elovskyTEST',
-  db: 'mongodb+srv://admin:admin@cluster0-p8qaw.mongodb.net/elovskyTEST?retryWrites=true&w=majority'
+  db: 'mongodb://localhost:27017/elovskyTEST',
+  clouddb: 'mongodb+srv://admin:admin@cluster0-p8qaw.mongodb.net/elovskyTEST?retryWrites=true&w=majority'
 };
