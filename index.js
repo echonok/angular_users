f3ed892fd36b625b849a49fb04aadf14e0e47f65
Nick Elovsky <echonok@gmail.com>
2020-03-06 15:29:09 +0300

    connection to db

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./config/db');
 
 const app = express();
 
@@ -10,6 +11,17 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json());
+
+mongoose.connect(config.db, { useUnifiedTopology: true, useNewUrlParser: true });
+
+mongoose.connection.on('connected', () => {
+  console.log('connected!');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.log('error!', error);
+});
 
 
 app.get('/', (req, res) => {
